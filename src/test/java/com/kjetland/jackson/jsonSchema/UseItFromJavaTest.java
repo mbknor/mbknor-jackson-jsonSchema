75bd80d5d476d96ef732a5c7fd6250bf4daf2cc0
Morten Kjetland <mbk@kjetland.com>
2016-10-13 10:29:48 +0200

    Added UseItFromJavaTest - This needs to be improved

---
+++ src/test/java/com/kjetland/jackson/jsonSchema/UseItFromJavaTest.java
@@ -0,0 +1,22 @@
+package com.kjetland.jackson.jsonSchema;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import scala.None;
+import scala.Option;
+
+public class UseItFromJavaTest {
+
+    static class MyJavaPojo {
+        public String name;
+    }
+
+    public UseItFromJavaTest() {
+        // Just make sure it compiles
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonSchemaGenerator g1 = new JsonSchemaGenerator(objectMapper);
+        // TODO - This is not very beautiful from Java - Need to improve Java API
+        g1.generateJsonSchema(MyJavaPojo.class, Option.apply(null), Option.apply(null));
+        g1.generateJsonSchema(MyJavaPojo.class, Option.apply("My title"), Option.apply("My description"));
+    }
+
+}
