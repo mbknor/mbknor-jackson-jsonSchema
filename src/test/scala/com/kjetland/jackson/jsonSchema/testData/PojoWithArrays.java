cbc7dd90b6defce30fdfdafbdd2053a2e81475cc
Morten Kjetland <mbk@kjetland.com>
2016-10-24 23:57:10 +0200

    Ref #14 - Added testcase that shows the issue

--- src/test/scala/com/kjetland/jackson/jsonSchema/testData/PojoWithArrays.java
+++ src/test/scala/com/kjetland/jackson/jsonSchema/testData/PojoWithArrays.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 public class PojoWithArrays {
 
+    // It was difficult to construct this from scala :)
+    public static List<List<String>> _listOfListOfStringsValues = Arrays.asList(Arrays.asList("1","2"), Arrays.asList("3"));
+
     public int[] intArray1;
     public String[] stringArray;
 
@@ -14,16 +17,19 @@ public class PojoWithArrays {
     public Parent[] polymorphismArray;
     public List<ClassNotExtendingAnything> regularObjectList;
 
+    public List<List<String>> listOfListOfStrings;
+
     public PojoWithArrays() {
     }
 
-    public PojoWithArrays(int[] intArray1, String[] stringArray, List<String> stringList, List<Parent> polymorphismList, Parent[] polymorphismArray, List<ClassNotExtendingAnything> regularObjectList) {
+    public PojoWithArrays(int[] intArray1, String[] stringArray, List<String> stringList, List<Parent> polymorphismList, Parent[] polymorphismArray, List<ClassNotExtendingAnything> regularObjectList, List<List<String>> listOfListOfStrings) {
         this.intArray1 = intArray1;
         this.stringArray = stringArray;
         this.stringList = stringList;
         this.polymorphismList = polymorphismList;
         this.polymorphismArray = polymorphismArray;
         this.regularObjectList = regularObjectList;
+        this.listOfListOfStrings = listOfListOfStrings;
     }
 
     @Override
@@ -40,6 +46,8 @@ public class PojoWithArrays {
         if (polymorphismList != null ? !polymorphismList.equals(that.polymorphismList) : that.polymorphismList != null)
         if (regularObjectList != null ? !regularObjectList.equals(that.regularObjectList) : that.regularObjectList != null)
             return false;
+        if (listOfListOfStrings != null ? !listOfListOfStrings.equals(that.listOfListOfStrings) : that.listOfListOfStrings != null)
+            return false;
         // Probably incorrect - comparing Object[] arrays with Arrays.equals
         return Arrays.equals(polymorphismArray, that.polymorphismArray);
 
@@ -52,6 +60,7 @@ public class PojoWithArrays {
         result = 31 * result + (stringList != null ? stringList.hashCode() : 0);
         result = 31 * result + (polymorphismList != null ? polymorphismList.hashCode() : 0);
         result = 31 * result + (regularObjectList != null ? regularObjectList.hashCode() : 0);
+        result = 31 * result + (listOfListOfStrings != null ? listOfListOfStrings.hashCode() : 0);
         result = 31 * result + Arrays.hashCode(polymorphismArray);
         return result;
     }
